08f2b67ca03c070699b338fc1c14621ff6402524
matsu1213 <matsu1213mgal@gmail.com>
2023-08-06 18:47:09 +0900

    最適化

--- src/main/java/net/azisaba/everyoneisdead/PacketHandler.java
+++ src/main/java/net/azisaba/everyoneisdead/PacketHandler.java
@@ -6,7 +6,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
 public class PacketHandler extends PacketAdapter {
     public PacketHandler() {
@@ -15,11 +15,11 @@ public class PacketHandler extends PacketAdapter {
 
     @Override
     public void onPacketSending(PacketEvent e){
-        final PacketContainer packet = e.getPacket().deepClone();
-        final Entity entity = packet.getEntityModifier(e).read(0);
-        if(!(entity instanceof LivingEntity) || packet.getIntegers().read(0) == e.getPlayer().getEntityId()){
+        final Entity entity = e.getPacket().getEntityModifier(e).read(0);
+        if(!(entity instanceof Player) || entity.getEntityId() == e.getPlayer().getEntityId()){
             return;
         }
+        final PacketContainer packet = e.getPacket().deepClone();
         final WrappedDataWatcher dataWatcher = WrappedDataWatcher.getEntityWatcher(entity).deepClone();
         dataWatcher.setObject(7, Float.NaN);
         packet.getWatchableCollectionModifier().write(0, dataWatcher.getWatchableObjects());
